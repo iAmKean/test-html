66178c30e1015d227c5f71b1d7647ec7712f2a4c
keno <keno@thinkbitsolutions.com>
2023-10-05 16:10:01 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div class="btn-wrapper">
+      <div class="btn" @click.prevent="">
+        <div @click="clickWithPreventDefault()">with @click.prevent=""</div>
+      </div>
+
+      <div style="margin-top: 2rem" class="btn">
+        <div @click="clickWithoutPreventDefault()">no @click.prevent=""</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+  },
+  mounted() {},
+  methods: {
+    clickWithPreventDefault() {
+      console.log("with @click.prevent")
+      alert('with')
+    },
+    clickWithoutPreventDefault() {
+      console.log("no @click.prevent")
+      alert('no')
+    }
+  }
+};
+</script>
+
+<style>
+.btn {
+  height: 50px;
+  width: 100%;
+  border: 1px solid gray;
+  position: relative;
+}
+
+.btn>div {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  height: 100%;
+  line-height: 50px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding: 1rem;
+  height: 100vh;
+}
+</style>
