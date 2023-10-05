f86620d66cc0c1d01a42d82eac48547db693992a
keno <keno@thinkbitsolutions.com>
2023-10-05 16:16:41 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
+  // devServer: {
+  //   proxy: {
+  //     "/": {
+  //       target: "",
+  //       // secure: true,
+  //       changeOrigin: true,
+  //       ws: false,
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // },
 })
